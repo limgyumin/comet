54024869deab4e87a372a7b996db12ec27e378cc
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-04 13:45:05 +0900

    [ADD] Register Page

---
+++ src/components/InputData/InputData.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, withStyles } from "@material-ui/core";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    boxShadow: "none",
+    color: theme.palette.getContrastText("#0080FF"),
+    fontFamily: "Noto Sans KR",
+    fontWeight: 300,
+    backgroundColor: "#0080FF",
+    width: 120,
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "#0080FF",
+    },
+  },
+}))(Button);
+
+const InputData = () => {
+  return (
+    <div>
+      <div className="contents">
+        <h1 className="big_title">Hello, There!</h1>
+        <h3 className="small_title">
+          Comet은 당신의 1일 1커밋을 도와드립니다.
+        </h3>
+        <ColorButton className="start_button" variant="contained">
+          시작하기
+        </ColorButton>
+      </div>
+    </div>
+  );
+};
+
+export default InputData;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
-import { Button } from "@material-ui/core";
+import background from "../../assets/images/background.svg";
+import StartSection from "../StartSection/StartSection";
 
 const Register = () => {
+  const [click, setClick] = useState(false);
   return (
     <div className="register">
-      <h1 className="big_title">Hello, There!</h1>
-      <h3 className="small_title">Comet은 당신의 1일 1커밋을 도와드립니다.</h3>
-      <Button className="start_button" variant="contained">
-        시작하기
-      </Button>
+      <StartSection />
+      <div className="background">
+        <img src={background} alt={background} />
+      </div>
     </div>
   );
 };
