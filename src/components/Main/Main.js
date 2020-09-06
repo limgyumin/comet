e79098a39c44fb737e62afe40cf23c063fa68d44
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-06 17:51:38 +0900

    [ADD] Loading Screen

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,11 +3,19 @@ import { observer } from "mobx-react";
 import stores from "../../stores";
 import { useQuery } from "@apollo/react-hooks";
 import GET_GITHUB_API from "../../assets/api/gql/userInfoQuery";
+
 import Header from "components/Header";
+import ShowTotalCommit from "components/ShowTotalCommit";
+import ReactLoading from "react-loading";
+import "./Main.css";
 
 const Main = observer(() => {
   const { userInfo, handleUserData, getUserId } = stores.UserStore;
   const { data, loading, error } = useQuery(GET_GITHUB_API(getUserId()));
+
+  if (error) {
+    console.log(error);
+  }
 
   useEffect(() => {
     if (!loading) {
@@ -16,9 +24,21 @@ const Main = observer(() => {
   }, [handleUserData, loading]);
 
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      {!loading && userInfo ? (
+        <ShowTotalCommit />
+      ) : (
+        <div className="loader">
+          <ReactLoading
+            type={"bars"}
+            color={"#EEEEEE"}
+            height={"8%"}
+            width={"8%"}
+          />
+        </div>
+      )}
+    </>
   );
 });
 
