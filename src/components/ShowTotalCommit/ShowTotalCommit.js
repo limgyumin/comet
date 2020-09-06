afb3789484fb493c2201306629b86363e823415a
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-06 17:53:41 +0900

    [ADD] Show Total Commit

---
+++ src/components/ShowTotalCommit/ShowTotalCommit.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { observer } from "mobx-react";
+import stores from "../../stores";
+import "./ShowTotalCommit.css";
+import CountUp from "react-countup";
+import FadeIn from "react-fade-in";
+
+const ShowTotalCommit = observer(() => {
+  const { userInfo } = stores.UserStore;
+
+  const {
+    contributionsCollection: {
+      contributionCalendar: { totalContributions },
+    },
+  } = userInfo;
+
+  return (
+    <>
+      <div className="container">
+        <FadeIn>
+          <div className="show_commit">
+            <h1 className="show_total">전체 커밋</h1>
+            <CountUp end={totalContributions} className="show_count" />
+            <h1 className="show_unit">회</h1>
+          </div>
+          <h3 className="show_goal">동해물과 백두산이 마르고 닳도록</h3>
+        </FadeIn>
+      </div>
+    </>
+  );
+});
+
+export default ShowTotalCommit;
