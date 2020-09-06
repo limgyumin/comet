c4fb6506203fbea640cc5b3c4f4afb88c28f0c6d
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-06 22:11:03 +0900

    [ADD] Start Page Fade In Animation

--- src/components/StartSection/StartSection.js
+++ src/components/StartSection/StartSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./StartSection.css";
+import FadeIn from "react-fade-in";
 import { Button, withStyles } from "@material-ui/core";
 
 const ColorButton = withStyles((theme) => ({
@@ -19,8 +20,8 @@ const ColorButton = withStyles((theme) => ({
 
 const StartSection = ({ setIsStarted }) => {
   return (
-    <div>
-      <div className="contents">
+    <div className="contents">
+      <FadeIn>
         <h1 className="big_title">Hello, There!</h1>
         <h3 className="small_title">
           Comet은 당신의 1일 1커밋을 도와드립니다.
@@ -33,7 +34,7 @@ const StartSection = ({ setIsStarted }) => {
         >
           시작하기
         </ColorButton>
-      </div>
+      </FadeIn>
     </div>
   );
 };
