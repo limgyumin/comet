a12063b0f76c1325443073ba73e7a6886ca379ce
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-06 19:43:31 +0900

    [ADD] Today Commit Style

--- src/components/ShowTodayCommit/ShowTodayCommit.js
+++ src/components/ShowTodayCommit/ShowTodayCommit.js
@@ -3,8 +3,8 @@ import { observer } from "mobx-react";
 import stores from "../../stores";
 import "./ShowTodayCommit.css";
 
-// import CountUp from "react-countup";
-// import FadeIn from "react-fade-in";
+import CountUp from "react-countup";
+import FadeIn from "react-fade-in";
 
 const ShowTodayCommit = observer(() => {
   const { userInfo, todayCommit, setTodayCommit } = stores.UserStore;
@@ -31,8 +31,13 @@ const ShowTodayCommit = observer(() => {
   }, [todayCommit]);
 
   return (
-    <div>
-      <h1>{todayCommit}</h1>
+    <div className="today_container">
+      <div className="today_title">
+        <h1 className="today_commit">오늘 커밋</h1>
+        {todayCommit && <CountUp end={todayCommit} className="today_count" />}
+        <h1 className="today_unit">회</h1>
+      </div>
+      <h3 className="today_comment">훌륭해요! 이대로 쭉 갑시다!</h3>
     </div>
   );
 });
