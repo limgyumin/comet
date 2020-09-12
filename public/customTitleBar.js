b58ce739724b261849acfba733cbfb36a170c810
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-12 16:07:55 +0900

    [ADD] Custom Title Bar

---
+++ public/customTitleBar.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const url = require("url");
+const customTitlebar = require("custom-electron-titlebar");
+
+window.addEventListener("DOMContentLoaded", () => {
+  new customTitlebar.Titlebar({
+    backgroundColor: customTitlebar.Color.fromHex("#24292e"),
+    icon: url.format(path.join(__dirname, "/images", "/icon.png")),
+  });
+
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+});
