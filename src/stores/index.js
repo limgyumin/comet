ff78cb7715883bb2a8eed5e81e6be088d7df7485
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 23:53:16 +0900

    [UPDATE] Store Index.js

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,8 @@
 import UserStore from "./UserStore";
-import RegisterStore from "./Register/RegisterStore";
 import GitHubStore from "./GitHub/GitHubStore";
 
 const stores = {
   UserStore: new UserStore(),
-  RegisterStore: new RegisterStore(),
   GitHubStore: new GitHubStore(),
 };
 
