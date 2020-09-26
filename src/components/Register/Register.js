adf512eeba48911e92496096c4f16d96138dc6a1
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 20:44:30 +0900

    [ADD] Button BoxShadow

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -9,7 +9,6 @@ import github from "../../assets/images/github.svg";
 
 const ColorButton = withStyles((theme) => ({
   root: {
-    boxShadow: "none",
     color: theme.palette.getContrastText("#24292E"),
     fontFamily: "Noto Sans KR",
     fontWeight: 300,
@@ -18,6 +17,7 @@ const ColorButton = withStyles((theme) => ({
     backgroundColor: "#24292E",
     width: "15.4rem",
     height: "2.9rem",
+    boxShadow: "0 0 0.1 0.5rem rgba(0,0,0,.5)",
     "&:hover": {
       boxShadow: "none",
       backgroundColor: "#24292E",
