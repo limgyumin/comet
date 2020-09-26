5b46add70c6850312b28f88846b1127a28dfbb98
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 13:09:00 +0900

    [ADD] Pages

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterContainer from "../container/RegisterContainer/RegisterContainer";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <RegisterContainer />
+    </>
+  );
+};
+
+export default RegisterPage;
