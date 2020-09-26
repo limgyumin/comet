093bd7e22c87c96149b012a7d360f30300774333
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-27 02:14:44 +0900

    [ADD] IsWait

--- src/stores/UserInfo/UserInfoStore.js
+++ src/stores/UserInfo/UserInfoStore.js
@@ -4,6 +4,7 @@ import { autobind } from "core-decorators";
 @autobind
 class UserInfoStore {
   @observable userInfo = {};
+  @observable wait = false;
   @observable success = false;
 
   @action
@@ -13,6 +14,10 @@ class UserInfoStore {
   @action
   isSuccess = (success) => {
     this.success = success;
+  };
+  @action
+  isWait = (wait) => {
+    this.wait = wait;
   };
 }
 
