d35062f10b701b7bf18bdb7178936e4a2789fdb1
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 23:53:28 +0900

    [UPDATE] App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import Main from "./components/Main";
 import userIdVerification from "./lib/userIdVerification";
 import { Switch, Route, Redirect } from "react-router-dom";
-import * as Pages from "./pages";
 import GitHubAuth from "./components/GitHubAuth/GitHubAuth";
 import GitHubSuccess from "components/GitHubSuccess";
+import Register from "components/Register";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route path="/login" render={() => <Pages.RegisterPage />} />
+        <Route path="/login" render={() => <Register />} />
         <Route
           path="/"
           exact={true}
