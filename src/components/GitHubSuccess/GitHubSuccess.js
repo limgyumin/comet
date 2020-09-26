e279d5f90f9858a378bf1dd73b89ab699e53d0d1
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 18:04:13 +0900

    [ADD] React Loading

--- src/components/GitHubSuccess/GitHubSuccess.js
+++ src/components/GitHubSuccess/GitHubSuccess.js
@@ -9,6 +9,14 @@ const GitHubSuccess = () => {
       <FadeIn delay={200}>
         <p className="GitHubSuccess-Title">인증이 완료되었습니다.</p>
         <p className="GitHubSuccess-Subtitle">아주 잠시만 기다려주세요!</p>
+        <div className="GitHubSuccess-Loader">
+          <ReactLoading
+            type={"cylon"}
+            color={"#757b81"}
+            width={"40%"}
+            height={"40%"}
+          />
+        </div>
       </FadeIn>
     </div>
   );
