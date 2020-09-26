09e2572d97868ae1235bebda0cdedfffb9274637
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 14:34:32 +0900

    [FIX] Electron Settings

--- app/index.js
+++ app/index.js
@@ -1,16 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const isDev = require("electron-is-dev");
 
 let mainWindow;
 
-const createWindow = () => {
+function createWindow() {
   mainWindow = new BrowserWindow({
     minWidth: 960,
     minHeight: 660,
-    titleBarStyle: "hidden",
+    titleBarStyle: "customButtonsOnHover",
     title: "Comet",
-    frame: false,
+    frame: true,
     //show: false,
     webPreferences: {
       webviewTag: true,
@@ -18,22 +20,13 @@ const createWindow = () => {
       enableRemoteModule: true,
     },
   });
-
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
-
-  if (isDev) {
-    // Open the DevTools.
-    // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>')
-    // mainWindow.webContents.openDevTools()
-  }
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
-};
+  mainWindow.on("closed", () => (mainWindow = null));
+}
 
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
