25f1af83c2cd955fa94d5e0b610408ad687b3122
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 14:35:16 +0900

    [UPDATE] Register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,16 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./Register.css";
-import background from "../../assets/images/background.svg";
-import Start from "../Start/Start";
-import InputUserId from "../InputUserId/InputUserId";
+import { Button, withStyles } from "@material-ui/core";
+import FadeIn from "react-fade-in";
+import { useHistory } from "react-router-dom";
+import { observer } from "mobx-react";
+import stores from "../../stores";
 
-const Register = () => {
-  const [isStarted, setIsStarted] = useState(false);
+const ColorButton = withStyles((theme) => ({
+  root: {
+    boxShadow: "none",
+    color: theme.palette.getContrastText("#0080FF"),
+    fontFamily: "Noto Sans KR",
+    fontWeight: 300,
+    backgroundColor: "#0080FF",
+    width: "8rem",
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "#0080FF",
+    },
+  },
+}))(Button);
+
+const Register = observer(() => {
+  const [id, setId] = useState("");
+  const { handleUserId, setUserId } = stores.UserStore;
+  const history = useHistory();
+
+  const submitUserId = useCallback(() => {
+    handleUserId(id);
+    setUserId(id);
+    history.push("/");
+  }, [id, handleUserId]);
+
   return (
-    <div className="register">
-      {isStarted ? <InputUserId /> : <Start setIsStarted={setIsStarted} />}
+    <div className="contents">
+      <FadeIn>
+        <h1 className="big_title">시작하기 앞서서</h1>
+        <h3 className="small_title">
+          Comet은 당신의 GitHub 아이디를 필요로 합니다.
+        </h3>
+        <ColorButton variant="contained" onClick={() => submitUserId()}>
+          완료
+        </ColorButton>
+      </FadeIn>
     </div>
   );
-};
+});
 
 export default Register;
