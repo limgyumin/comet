e2162c9c3dda4c1a957e9522ef9e0be4b4b75f30
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-26 17:46:33 +0900

    [ADD] Success Message

--- src/components/GitHubSuccess/GitHubSuccess.js
+++ src/components/GitHubSuccess/GitHubSuccess.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./GitHubSuccess.css";
+import FadeIn from "react-fade-in";
+import ReactLoading from "react-loading";
 
 const GitHubSuccess = () => {
   return (
-    <div>
-      <h3>인증이 완료되었습니다.</h3>
+    <div className="GitHubSuccess">
+      <FadeIn delay={200}>
+        <p className="GitHubSuccess-Title">인증이 완료되었습니다.</p>
+        <p className="GitHubSuccess-Subtitle">아주 잠시만 기다려주세요!</p>
+      </FadeIn>
     </div>
   );
 };
