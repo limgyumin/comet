17b49fca6df0643ba7e6ef7d0f5de3c1c06de567
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 18:10:13 +0900

    [UPDATE] pages index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export { default as MainPage } from "./MainPage";
 export { default as RegisterPage } from "./RegisterPage";
-export { default as GitHubSuccessPage } from "./GitHubSuccessPage";
 export { default as GitHubAuthPage } from "./GitHubAuthPage";
