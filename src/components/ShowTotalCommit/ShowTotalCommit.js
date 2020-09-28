5e24d84d7551b28f9af45af144b4ee48ab054845
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 20:50:10 +0900

    [FIX] Get Props from Main

--- src/components/ShowTotalCommit/ShowTotalCommit.js
+++ src/components/ShowTotalCommit/ShowTotalCommit.js
@@ -1,41 +1,28 @@
 import React from "react";
-import { observer } from "mobx-react";
-import stores from "../../stores";
 import "./ShowTotalCommit.css";
 
 import CountUp from "react-countup";
 import FadeIn from "react-fade-in";
 
-const ShowTotalCommit = observer(() => {
-  const { userInfo, goalCommit, setGoalCommit } = stores.UserStore;
-
-  const {
-    contributionsCollection: {
-      contributionCalendar: { totalContributions },
-    },
-  } = userInfo;
-
-  if (totalContributions < 100) {
-    setGoalCommit(Math.ceil(totalContributions / 10) * 10);
-  } else {
-    setGoalCommit(Math.ceil(totalContributions / 100) * 100);
-  }
-
+const ShowTotalCommit = ({ totalCommit, goalCommit }) => {
   return (
-    <div className="total_container">
-      <FadeIn>
-        <div className="total_title">
-          <h1 className="total_commit">전체 커밋</h1>
-          <CountUp end={totalContributions} className="total_count" />
-          <h1 className="total_unit">회</h1>
+    <>
+      {totalCommit && (
+        <div className="total_container">
+          <FadeIn>
+            <div className="total_title">
+              <h1 className="total_commit">전체 커밋</h1>
+              <CountUp end={totalCommit} className="total_count" />
+              <h1 className="total_unit">회</h1>
+            </div>
+            <h3 className="total_goal">
+              {goalCommit} 커밋 까지 {goalCommit - totalCommit} 커밋 남았습니다.
+            </h3>
+          </FadeIn>
         </div>
-        <h3 className="total_goal">
-          {goalCommit} 커밋 까지 {goalCommit - totalContributions} 커밋
-          남았습니다.
-        </h3>
-      </FadeIn>
-    </div>
+      )}
+    </>
   );
-});
+};
 
 export default ShowTotalCommit;
--- src/components/ShowWeekCommit/ShowWeekCommit.js
+++ src/components/ShowWeekCommit/ShowWeekCommit.js
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
-import { observer } from "mobx-react";
-import stores from "../../stores";
+import React from "react";
 import "./ShowWeekCommit.css";
 import chart from "../../assets/images/chart.svg";
 import barChart from "../../assets/images/bar-chart.svg";
@@ -8,65 +6,45 @@ import barChart from "../../assets/images/bar-chart.svg";
 import CountUp from "react-countup";
 import FadeIn from "react-fade-in";
 
-const ShowWeekCommit = observer(() => {
-  const { userInfo, weekCommit, setWeekCommit } = stores.UserStore;
-  const {
-    contributionsCollection: {
-      contributionCalendar: { weeks },
-    },
-  } = userInfo;
-
-  let commitCount = 0;
-
-  const getWeekCommit = () => {
-    weeks.map((week, index) => {
-      if (index === weeks.length - 1) {
-        week.contributionDays.map((day, index2) => {
-          commitCount += day.contributionCount;
-        });
-        setWeekCommit(commitCount);
-      }
-    });
-  };
-
-  useEffect(() => {
-    getWeekCommit();
-  }, [weekCommit]);
-
+const ShowWeekCommit = ({ weekCommit, weekAvg }) => {
   return (
-    <FadeIn>
-      <div className="week_area">
-        <div className="week_container">
-          <FadeIn>
-            <div className="week_title">
-              <img src={chart} alt={chart} className="chart_icon" />
-              <h1 className="week_commit">이번 주 커밋</h1>
-              {weekCommit > 0 ? (
-                <CountUp end={weekCommit} className="week_count" />
-              ) : (
-                <h1 className="week_no_commit">{weekCommit}</h1>
-              )}
-              <h1 className="week_unit">회</h1>
+    <>
+      {weekAvg && (
+        <FadeIn>
+          <div className="week_area">
+            <div className="week_container">
+              <FadeIn>
+                <div className="week_title">
+                  <img src={chart} alt={chart} className="chart_icon" />
+                  <h1 className="week_commit">이번 주 커밋</h1>
+                  {weekCommit > 0 ? (
+                    <CountUp end={weekCommit} className="week_count" />
+                  ) : (
+                    <h1 className="week_no_commit">{weekCommit}</h1>
+                  )}
+                  <h1 className="week_unit">회</h1>
+                </div>
+              </FadeIn>
             </div>
-          </FadeIn>
-        </div>
-        <div className="week_container2">
-          <FadeIn>
-            <div className="week_title">
-              <img src={barChart} alt={barChart} className="chart_icon" />
-              <h1 className="week_commit">이번 주 평균 커밋</h1>
-              {weekCommit > 0 ? (
-                <CountUp end={weekCommit / 7} className="week_count" />
-              ) : (
-                <h1 className="week_no_commit">{weekCommit}</h1>
-              )}
-              <h1 className="week_unit">회</h1>
+            <div className="week_container2">
+              <FadeIn>
+                <div className="week_title">
+                  <img src={barChart} alt={barChart} className="chart_icon" />
+                  <h1 className="week_commit">이번 주 평균 커밋</h1>
+                  {weekCommit > 0 ? (
+                    <CountUp end={weekAvg} className="week_count" />
+                  ) : (
+                    <h1 className="week_no_commit">{weekCommit}</h1>
+                  )}
+                  <h1 className="week_unit">회</h1>
+                </div>
+              </FadeIn>
             </div>
-          </FadeIn>
-        </div>
-      </div>
-    </FadeIn>
+          </div>
+        </FadeIn>
+      )}
+    </>
   );
-});
+};
 
 export default ShowWeekCommit;
