94623850c5589b8c43600fd2e3436e67548880ea
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 12:22:08 +0900

    [ADD] Random Title

--- src/components/GitHubSuccess/GitHubSuccess.js
+++ src/components/GitHubSuccess/GitHubSuccess.js
@@ -2,29 +2,10 @@ import React, { useEffect } from "react";
 import "./GitHubSuccess.css";
 import FadeIn from "react-fade-in";
 import ReactLoading from "react-loading";
-import { useLocation } from "react-router-dom";
 import stores from "../../stores";
 import { observer } from "mobx-react";
 
-const GitHubSuccess = observer(({ setCode }) => {
-  const { isWait } = stores.UserInfoStore;
-  const location = useLocation();
-
-  const getUrlParams = () => {
-    var params = {};
-    location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, (str, key, value) => {
-      params[key] = value;
-    });
-    return params;
-  };
-
-  useEffect(() => {
-    const params = getUrlParams();
-    console.log(params.code);
-    setCode(params.code);
-    isWait(true);
-  }, []);
-
+const GitHubSuccess = observer(() => {
   return (
     <div className="GitHubSuccess">
       <FadeIn delay={200}>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,47 +1,19 @@
 import React, { useState, useCallback } from "react";
 import "./Register.css";
-import { Button, withStyles } from "@material-ui/core";
 import FadeIn from "react-fade-in";
 import { Link, useHistory } from "react-router-dom";
 import { observer } from "mobx-react";
 import stores from "../../stores";
 import github from "../../assets/images/github.svg";
+import BoxShadowButton from "../common/BoxShadowButton";
 
-const ColorButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText("#24292E"),
-    fontFamily: "Noto Sans KR",
-    fontWeight: 300,
-    fontSize: "1rem",
-    letterSpacing: "0.6px",
-    backgroundColor: "#24292E",
-    width: "15.4rem",
-    height: "2.9rem",
-    boxShadow: "0 0 0.1 0.5rem rgba(0,0,0,.5)",
-    "&:hover": {
-      boxShadow: "none",
-      backgroundColor: "#24292E",
-    },
-  },
-}))(Button);
-
-const Register = observer(() => {
-  const [id, setId] = useState("");
-  const { handleUserId, setUserId } = stores.UserStore;
-  const history = useHistory();
-
-  const submitUserId = useCallback(() => {
-    handleUserId(id);
-    setUserId(id);
-    history.push("/");
-  }, [id, handleUserId]);
-
+const Register = ({ randomTitle }) => {
   return (
     <div className="Register">
       <div className="Register-Text">
         <FadeIn delay={300}>
           {/*랜덤 타이틀 만들 예정 */}
-          <h1 className="Register-Text-Title">Hello, There!</h1>
+          <h1 className="Register-Text-Title">{randomTitle()}</h1>
         </FadeIn>
         <FadeIn delay={500}>
           <h3 className="Register-Text-Subtitle">
@@ -52,20 +24,20 @@ const Register = observer(() => {
       <div className="Register-GitHub">
         <FadeIn delay={700}>
           <Link to="/auth" className="Register-GitHub-Link">
-            <ColorButton variant="contained" onClick={() => {}}>
+            <BoxShadowButton variant="contained" onClick={() => {}}>
               <img src={github} alt={github} className="Register-GitHub-Icon" />
-              GitHub으로 로그인
-            </ColorButton>
+              GitHub 아이디로 시작
+            </BoxShadowButton>
           </Link>
         </FadeIn>
         <FadeIn delay={900}>
           <p className="Register-GitHub-Text">
-            Comet을 사용하시려면 GitHub 계정이 필요합니다.
+            Comet을 사용하시려면 GitHub 아이디가 필요합니다.
           </p>
         </FadeIn>
       </div>
     </div>
   );
-});
+};
 
 export default Register;
