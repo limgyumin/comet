74a8993aac624796035c276d11348ff34210ca5b
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 20:48:11 +0900

    [UPDATE] stores index.js

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,11 +1,7 @@
-import UserStore from "./UserStore";
-import UserInfoStore from "./UserInfo/UserInfoStore";
 import GitHubAuthStore from "./GitHubAuth/GitHubAuthStore";
 import MainStore from "./Main/MainStore";
 
 const stores = {
-  UserStore: new UserStore(),
-  UserInfoStore: new UserInfoStore(),
   GitHubAuthStore: new GitHubAuthStore(),
   MainStore: new MainStore(),
 };
