f1d9bd004a2f05e740200d4e9ae13314f73cc01f
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 12:16:18 +0900

    [UPDATE] CssTextField

--- src/components/common/CssTextField.js
+++ src/components/common/CssTextField.js
@@ -4,15 +4,16 @@ export default withStyles({
   root: {
     width: "20rem",
     height: "2.9rem",
+    marginBottom: "0.4rem",
     "& label.Mui-focused": {
-      color: "#0077FF",
+      color: "#191B1D",
     },
     "& .MuiInput-underline:after": {
-      borderBottomColor: "#0077FF",
+      borderBottomColor: "#191B1D",
     },
     "& .MuiOutlinedInput-root": {
       "&.Mui-focused fieldset": {
-        borderColor: "#0077FF",
+        borderColor: "#191B1D",
       },
     },
   },
