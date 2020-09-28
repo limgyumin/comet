f9fe91894cc9f45f0a572ff97ab0d1ab131cf327
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-09-28 20:49:52 +0900

    [UPDATE] Connect with Server Repository

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,54 +1,40 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { observer } from "mobx-react";
-import stores from "../../stores";
-import { useQuery } from "@apollo/react-hooks";
-import GET_GITHUB_API from "../../assets/api/gql/userInfoQuery";
-import { useHistory, withRouter } from "react-router-dom";
 
 import "./Main.css";
 import Header from "components/Header";
-import ReactLoading from "react-loading";
 import ShowTotalCommit from "components/ShowTotalCommit";
 import ShowTodayCommit from "components/ShowTodayCommit";
 import ShowWeekCommit from "components/ShowWeekCommit";
 
-const Main = observer(() => {
-  const { userInfo, handleUserData, getUserId } = stores.UserStore;
-  const { data, loading, error } = useQuery(GET_GITHUB_API(getUserId()));
-  const history = useHistory();
-
-  if (error) {
-    localStorage.removeItem("userId");
-    history.push("/");
+const Main = observer(
+  ({
+    profile,
+    name,
+    bio,
+    totalCommit,
+    goalCommit,
+    todayCommit,
+    weekCommit,
+    weekAvg,
+    sliceText,
+    logout,
+  }) => {
+    return (
+      <>
+        <Header
+          profile={profile}
+          name={name}
+          bio={bio}
+          logout={logout}
+          sliceText={sliceText}
+        />
+        <ShowTotalCommit totalCommit={totalCommit} goalCommit={goalCommit} />
+        <ShowTodayCommit todayCommit={todayCommit} />
+        <ShowWeekCommit weekCommit={weekCommit} weekAvg={weekAvg} />
+      </>
+    );
   }
+);
 
-  useEffect(() => {
-    if (!loading) {
-      handleUserData(data);
-    }
-  }, [handleUserData, loading]);
-
-  return (
-    <>
-      <Header />
-      {!loading && userInfo ? (
-        <>
-          <ShowTotalCommit />
-          <ShowTodayCommit />
-          <ShowWeekCommit />
-        </>
-      ) : (
-        <div className="main_loader">
-          <ReactLoading
-            type={"bars"}
-            color={"#DEDEDE"}
-            height={"8%"}
-            width={"8%"}
-          />
-        </div>
-      )}
-    </>
-  );
-});
-
-export default withRouter(Main);
+export default Main;
