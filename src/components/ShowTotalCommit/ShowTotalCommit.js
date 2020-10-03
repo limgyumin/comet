82dac3ce3e82468b3692707d55879f1c5f39df74
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-03 14:33:13 +0900

    [ADD] Animated Circular ProgressBar, Message

--- src/components/ShowTotalCommit/ShowTotalCommit.js
+++ src/components/ShowTotalCommit/ShowTotalCommit.js
@@ -1,11 +1,71 @@
 import React from "react";
 import "./ShowTotalCommit.css";
+import barChart from "../../assets/icons/bar-chart.svg";
+import AnimatedProgressProvider from "../common/Animated/AnimatedProgressProvider";
+import { easeQuadInOut } from "d3-ease";
 
-import CountUp from "react-countup";
-import FadeIn from "react-fade-in";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const ShowTotalCommit = ({ totalCommit, goalCommit }) => {
-  return <>{totalCommit >= 0 && <div className="ShowTotal"></div>}</>;
+  return (
+    <>
+      {totalCommit >= 0 && (
+        <div className="ShowTotal">
+          <div className="ShowTotal-Header">
+            <img src={barChart} alt={barChart} />
+            <p className="ShowTotal-Header-Title">전체 커밋 수</p>
+          </div>
+          <div className="ShowTotal-Header-ProgressBar">
+            <AnimatedProgressProvider
+              valueStart={0}
+              valueEnd={totalCommit}
+              duration={1.4}
+              easingFunction={easeQuadInOut}
+            >
+              {(value) => {
+                const roundedValue = Math.round(value);
+                return (
+                  <CircularProgressbar
+                    value={roundedValue}
+                    maxValue={goalCommit}
+                    text={`${roundedValue}`}
+                    strokeWidth={6}
+                    styles={{
+                      root: {
+                        height: "7.6rem",
+                        width: "7.6rem",
+                      },
+                      path: {
+                        stroke: "#191b1d",
+                        strokeLinecap: "butt",
+                        transition: "none",
+                      },
+                      trail: {
+                        stroke: "#999A9B",
+                        strokeLinecap: "butt",
+                      },
+                      text: {
+                        fill: "#191b1d",
+                        fontSize: "16px",
+                        fontWeight: 500,
+                      },
+                      background: {
+                        fill: "#3e98c7",
+                      },
+                    }}
+                  />
+                );
+              }}
+            </AnimatedProgressProvider>
+            <p className="ShowTotal-Header-ProgressBar-Text">
+              {goalCommit} 커밋까지 {goalCommit - totalCommit} 커밋 남았습니다.
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShowTotalCommit;
